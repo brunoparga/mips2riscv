ff88d47e25ba0f7bbf1ae845cf25732f1bb07489
Bruno Parga <brunoparga@gmail.com>
2023-07-10 13:37:53 +0200

    Run ch. 26 code

---
+++ 26 - Simple Subroutine Linkage/examples/readInt.asm
@@ -0,0 +1,24 @@
+# readInt -- prompt for and read an integer
+#
+# on entry:
+#	$ra -- return address
+#
+# on exit:
+#	$v0 -- the integer
+
+	.text
+	.globl	readInt
+readInt:
+	la	$a0, prompt	# print string
+	li	$v0, 4		# service 4
+	syscall
+
+	li	$v0, 5		# read int into $v0
+	syscall		 	# service 5
+
+	jr	$ra		# return
+	nop			# branch delay slot
+
+	.data
+prompt:
+	.asciiz "Enter an integer: "
